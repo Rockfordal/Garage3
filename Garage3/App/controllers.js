da722d6132f3d97e569499e1144c31a734b4317b
Anders Lundström <lundstrom123@gmail.com>
2016-12-01 11:36:15 +0100

    Angular

---
+++ Garage3/App/controllers.js
@@ -0,0 +1,13 @@
+﻿angular.module("garage", ["GarageService"])
+    .controller("VehicleTypeCtrl", function ($scope, VehicleType) {
+        $scope.vehicletypes = VehicleType.query();
+})
+    .controller("VehicleCtrl", function ($scope, Vehicle) {
+        $scope.vehicles = Vehicle.query();
+})
+    .controller("PersonCtrl", function ($scope, Person) {
+        $scope.people = Person.query();
+})
+    .controller("OwnerCtrl", function ($scope, Owner) {
+        $scope.owners = Owner.query();
+})
---
+++ Garage3/App/services.js
@@ -0,0 +1,32 @@
+﻿angular.module("GarageService", ["ngResource"])
+       .factory("VehicleType", function ($resource) {
+           return $resource(
+               "/api/vehicletypes/:Id",
+               { Id: "@Id" },
+               { "update": { method: "PUT" } }
+          );
+       })
+
+       .factory("Vehicle", function ($resource) {
+           return $resource(
+               "/api/vehicles/:Id",
+               { Id: "@Id" },
+               { "update": { method: "PUT" } }
+          );
+       })
+
+       .factory("Person", function ($resource) {
+           return $resource(
+               "/api/people/:Id",
+               { Id: "@Id" },
+               { "update": { method: "PUT" } }
+          );
+       })
+
+       .factory("Owner", function ($resource) {
+           return $resource(
+               "/api/owners/:Id",
+               { Id: "@Id" },
+               { "update": { method: "PUT" } }
+          );
+       });
