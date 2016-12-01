b8b7e12f49b563301a12eb809c9aa108f8188342
Anders Lundström <lundstrom123@gmail.com>
2016-12-01 09:38:38 +0100

    Fixat så Seeds funkar från tom databas

--- Garage3/Migrations/Configuration.cs
+++ Garage3/Migrations/Configuration.cs
@@ -24,25 +24,27 @@ namespace Garage3.Migrations
             c.db_VehicleTypes.AddOrUpdate(vt => vt.Type, _truck);
             c.db_VehicleTypes.AddOrUpdate(vt => vt.Type, _boat);
             c.db_VehicleTypes.AddOrUpdate(vt => vt.Type, _bike);
-            //var car   = c.db_VehicleTypes.Single(v => v.Type == "Car");
-            //var truck = c.db_VehicleTypes.Single(v => v.Type == "Truck");
-            //var boat  = c.db_VehicleTypes.Single(v => v.Type == "Boat");
-            //var bike  = c.db_VehicleTypes.Single(v => v.Type == "Bike");
+            c.SaveChanges();
+            var car   = c.db_VehicleTypes.SingleOrDefault(v => v.Type == "Car");
+            var truck = c.db_VehicleTypes.SingleOrDefault(v => v.Type == "Truck");
+            var boat  = c.db_VehicleTypes.SingleOrDefault(v => v.Type == "Boat");
+            var bike  = c.db_VehicleTypes.SingleOrDefault(v => v.Type == "Bike");
 
-            //// Fordon
-            //var v1 = new Vehicle { RegNr = "KSU583", Color = "Vit",   NumberOfWheels = 4, Manufacturer = "Volvo", Model = "240",     VehicleType = car };
-            //var v2 = new Vehicle { RegNr = "AUD001", Color = "Svart", NumberOfWheels = 4, Manufacturer = "Audi",  Model = "Quattro", VehicleType = car };
-            //c.db_Vehicles.AddOrUpdate(v => v.RegNr, v1, v2);
+            // Fordon
+            var v1 = new Vehicle { RegNr = "KSU583", Color = "Vit", NumberOfWheels = 4, Manufacturer = "Volvo", Model = "240", VehicleType = car };
+            var v2 = new Vehicle { RegNr = "AUD001", Color = "Svart", NumberOfWheels = 4, Manufacturer = "Audi", Model = "Quattro", VehicleType = car };
+            c.db_Vehicles.AddOrUpdate(v => v.RegNr, v1, v2);
 
-            //// Personer
-            //var p1 = new Person { FirstName = "Steve", LastName = "Smith", SSN = "123456789" };
-            //var p2 = new Person { FirstName = "Benny", LastName = "hill",  SSN = "8765432101" };
-            //c.db_Persons.AddOrUpdate(p => p.SSN, p1, p2);
+            // Personer
+            var p1 = new Person { FirstName = "Steve", LastName = "Smith", SSN = "123456789" };
+            var p2 = new Person { FirstName = "Benny", LastName = "hill",  SSN = "8765432101" };
+            c.db_Persons.AddOrUpdate(p => p.SSN, p1, p2);
+            c.SaveChanges();
 
-            //// Ägare
-            //var o1 = new Owner { PersonId = p1.Id };
-            //var o2 = new Owner { PersonId = p2.Id };
-            //c.db_Owners.AddOrUpdate(o => o.PersonId, o1, o2);
+            // Ägare
+            var o1 = new Owner { PersonId = p1.Id };
+            var o2 = new Owner { PersonId = p2.Id };
+            c.db_Owners.AddOrUpdate(o => o.PersonId, o1, o2);
         }
     }
 }
