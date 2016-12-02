a3422b0d7f92e91f3b47e1d04cced298e3c89928
Anders Lundström <anders@urkraft.se>
2016-12-02 04:44:22 +0100

    en navbar komponent

---
+++ Garage3/App/navbar/index.js
@@ -0,0 +1,25 @@
+(function () {
+    "use strict"
+
+    angular.module("garage").component('menuBar', {
+        bindings: {
+            brand: '<'
+        },
+        templateUrl: '/App/navbar/index.html',
+        controllerAs: "model",
+        controller: function() {
+            // this.brand = "Garage";
+            this.menus = [{
+                name: "Fordon",
+                state: "fordon"
+            }, {
+                name: "Personer",
+                state: "people"
+            }, {
+                name: "FordonsTyper",
+                state: "vehicletypes"
+            }];
+        }
+    });
+
+})();
