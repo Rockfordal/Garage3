dd762cc1c7c776e7ca3596a08668ac22dc984ea5
Anders Lundström <lundstrom123@gmail.com>
2016-12-05 15:26:53 +0100

    Common tjänst för att lägga till entity

---
+++ Garage3/App/services/common.js
@@ -0,0 +1,15 @@
+﻿angular.module("garage.common", [])
+    .service("Common",  function() {
+        this.AddEntity = AddEntity
+    }
+);
+
+function AddEntity(newEntity, entities) {
+    newEntity.$save(function (data, putResponseHeaders) {
+        //console.log('add success: ', data);
+        entities.push(newEntity);
+    }, function (res) {
+        console.log('fel vid skapa. modelState: ', res.data.ModelState);
+    });
+    return null;
+};
