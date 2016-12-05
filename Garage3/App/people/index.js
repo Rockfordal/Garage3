5baf614f6d565bc2367d38354d4a639057a5ad67
Anders Lundström <lundstrom123@gmail.com>
2016-12-05 15:30:53 +0100

    Lägg till ta bort på people

--- Garage3/App/people/index.js
+++ Garage3/App/people/index.js
@@ -5,9 +5,29 @@ angular.module("garage").component("peopleIndex", {
     templateUrl: "/App/people/index.html",
     bindings: { people: '<' },
     controllerAs: "model",
-    controller: function() {
-
-
+    controller: function (Person, Common) {
+        var vm = this;
+        this.add = function () {
+            var newPerson = new Person(vm.person);
+            Common.AddEntity(newPerson, vm.people);
+        }
+        this.remove = function (person, index) {
+            Person.delete({ id: person.Id }, function () {
+                vm.people.splice(index, 1);
+            });
+        }
+        this.startedit = function (person) {
+            vm.editing = true;
+            vm.person = person;
+        }
+        this.update = function () {
+            vm.person.$update();
+            vm.clear();
+        }
+        this.clear = function () {
+            vm.editing = false;
+            vm.person = new Person();
+        }
     }
 });
 })();
